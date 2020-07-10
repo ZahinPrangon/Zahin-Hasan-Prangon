60c4dba78f7436f6c2afff182ca310ab454c2281
Zahin430 <zhprangon@mun.ca>
2020-07-09 20:42:16 -0400

    Updates

---
+++ precache-manifest.d7b4c2f1b9da1e59bab8c95b5ee03983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "337c963ae0c723515dd80050be1efa5a",
+    "url": "/Zahin-Hasan-Prangon/index.html"
+  },
+  {
+    "revision": "9febad73f94086c57151",
+    "url": "/Zahin-Hasan-Prangon/static/css/main.f7ec9b6e.chunk.css"
+  },
+  {
+    "revision": "d23a2b82f392150f2068",
+    "url": "/Zahin-Hasan-Prangon/static/js/2.0047c582.chunk.js"
+  },
+  {
+    "revision": "9febad73f94086c57151",
+    "url": "/Zahin-Hasan-Prangon/static/js/main.6b59434c.chunk.js"
+  },
+  {
+    "revision": "d1c5aec3373208a057dc",
+    "url": "/Zahin-Hasan-Prangon/static/js/runtime~main.d8edcddf.js"
+  }
+]);
