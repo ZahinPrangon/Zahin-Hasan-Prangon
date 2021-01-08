fc42bd1dfec6b2ea0990a871e76421403114d14c
Zahin430 <zhprangon@mun.ca>
2021-01-07 21:24:13 -0330

    UPDATE on resume

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,107 +1,151 @@
 let resumeData = {
-  "imagebaseurl":"https://techonoeticsclub.github.io/",
-  "name": "Zahin Hasan Prangon",
-  "role": "",
-  "skypeid": "Your skypeid",
-  "roleDescription": "THINKER | DEVELOPER | STORYTELLER",
-  "socialLinks":[
-      {
-        "name":"linkedin",
-        "url":"https://www.linkedin.com/in/zahin-hasan-88543613a/",
-        "className":"fa fa-linkedin"
-      },
-      {
-        "name":"github",
-        "url":"https://github.com/Zahin430",
-        "className":"fa fa-github"
-      },
-      {
-        "name": "twitter",
-        "url": "https://twitter.com/Zahin431",
-        'className': "fa fa-twitter"
-      },
-      {
-        "name":"facebook",
-        "url": "https://www.facebook.com/prangon.xahin",
-        "className" : "fa fa-facebook"
-      }
-    ],
-  "aboutme":"I am from Bangladesh and moved to St Johns, NL on January 2017 in order to pursue my bachelors degree in Computer Science with a Minor in Business. I am extremely passionate about web and cross-platform mobile application development.",
-  "address":"St John's, Newfoundland and Labredor, Canada",
-  "website":"",
-  "email":"zhprangon@mun.ca",
-  "education":[
+  imagebaseurl: "https://techonoeticsclub.github.io/",
+  name: "Zahin Hasan Prangon",
+  role: "",
+  skypeid: "Your skypeid",
+  roleDescription: "THINKER | DEVELOPER | STORYTELLER",
+  socialLinks: [
     {
-      "UniversityName":"Memorial University of Newfoundland",
-      "specialization":"Bachelor of Science in Computer Science with Business Administration and Mathematics Minor",
-      "MonthOfPassing":"Jan, 2017",
-      "YearOfPassing":" to April, 2021",
-    }
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/zahin-hasan-88543613a/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/Zahin430",
+      className: "fa fa-github",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/Zahin431",
+      className: "fa fa-twitter",
+    },
+    {
+      name: "facebook",
+      url: "https://www.facebook.com/prangon.xahin",
+      className: "fa fa-facebook",
+    },
   ],
-  "work":[
+  aboutme:
+    "I am from Bangladesh and moved to St Johns, NL on January 2017 in order to pursue my bachelors degree in Computer Science with a Minor in Business. I am extremely passionate about web and cross-platform mobile application development.",
+  address: "St John's, Newfoundland and Labredor, Canada",
+  website: "",
+  email: "zhprangon@mun.ca",
+  education: [
     {
-      "CompanyName":"IBM Canada Ltd.",
-      "specialization":"Front-End Developer (Cloud) Co-op",
-      "start": "January, 2020",
-      "end":"present"
+      UniversityName: "Memorial University of Newfoundland",
+      specialization: "Bachelor of Science in Computer Science",
+      minor: "Business Administration",
+      MonthOfPassing: "Jan, 2017",
+      YearOfPassing: " to April, 2021",
     },
-    {
-      "CompanyName":"Neurofog Laboratory, Memorial University of Newfoundland",
-      "specialization":"Full Stack Developer",
-      "start": "June, 2019",
-      "end":"August, 2020"
-    },
-    {
-      "CompanyName":"Department of Chemistry, Memorial University of Newfoundland",
-      "specialization":"Web Application Developer",
-      "start": "January, 2019",
-      "end":"April, 2019"
-    },
-    {
-
-      "CompanyName":"Office of the Alumni Affairs, Memorial University of Newfoundland",
-      "specialization":"Database Assistant",
-      "start": "January, 2018",
-      "end":"April, 2018",
-    }
   ],
-  "portfolio":[
+  work: [
     {
-      "name":"ACTIVITY TRACKER",
-      "description":"A Java Desktop Application built using the Swing library as a school project.",
-      "imgurl":"images/running.jpeg",
-      "link":"https://github.com/Zahin430/cs2005"
-
+      CompanyName: "Bikemaps.org",
+      specialization: "Full Stack Software Developer",
+      start: "December, 2020",
+      end: "present",
+      tasks: [
+        "Achieved 18% increase in loading speed by upgrading and optimizing current web application to use Python 3, Django 3. ",
+        "Developing key features for the Bikemaps web application using Django, PostgreSQL, Leaftlet.",
+        "Planning and integrating new features and web application architecture for MobilityAccess(BikeMaps pedestrian) using React, Django & PostgreSQL.",
+      ],
     },
     {
-      "name":"VISITNL",
-      "description":"A NodeJS Web Application serves as a social media platform. Built using NodeJS and MongoDB",
-      "imgurl":"images/portfolio/project2.jpg",
-      "link":"https://github.com/Zahin430/VISITNL"
-
+      CompanyName:
+        "Department of Biology, Memorial University of Newfoundland.",
+      specialization: "Software Developer",
+      start: "September, 2020",
+      end: "December, 2020",
+      tasks: [
+        "Achieved 18% increase in loading speed by upgrading and optimizing current web application to use Python 3, Django 3. ",
+        "Developing key features for the Bikemaps web application using Django, PostgreSQL, Leaftlet.",
+        "Planning and integrating new features and web application architecture for MobilityAccess(BikeMaps pedestrian) using React, Django & PostgreSQL.",
+      ],
     },
     {
-      "name":"AngularRecipe",
-      "description":"An Angular Web Application that keeps a track of recipes being bought and sold.",
-      "imgurl":"images/angular.png",
-      "link":"https://github.com/Zahin430/RecipeBook"
-
+      CompanyName: "IBM Canada Ltd.",
+      specialization: "Software Developer (Cloud) Co-op",
+      start: "January, 2020",
+      end: "August, 2020",
+      tasks: [
+        "Achieved 18% increase in loading speed by upgrading and optimizing current web application to use Python 3, Django 3. ",
+        "Developing key features for the Bikemaps web application using Django, PostgreSQL, Leaftlet.",
+        "Planning and integrating new features and web application architecture for MobilityAccess(BikeMaps pedestrian) using React, Django & PostgreSQL.",
+      ],
     },
     {
-      "name":"Neurofog Website",
-      "description":"A React application built for the Neurofog Laboratory at Memorial University of Newfoundland",
-      "imgurl":"images/logo.png",
-      "link":"http://neurofog.ca/"
-
+      CompanyName: "Neurofog Laboratory, Memorial University of Newfoundland",
+      specialization: "Full Stack Developer",
+      start: "June, 2019",
+      end: "August, 2020",
+      tasks: [
+        "Achieved 18% increase in loading speed by upgrading and optimizing current web application to use Python 3, Django 3. ",
+        "Developing key features for the Bikemaps web application using Django, PostgreSQL, Leaftlet.",
+        "Planning and integrating new features and web application architecture for MobilityAccess(BikeMaps pedestrian) using React, Django & PostgreSQL.",
+      ],
     },
-
+    {
+      CompanyName:
+        "Department of Chemistry, Memorial University of Newfoundland",
+      specialization: "Web Application Developer",
+      start: "January, 2019",
+      end: "April, 2019",
+      tasks: [
+        "Achieved 18% increase in loading speed by upgrading and optimizing current web application to use Python 3, Django 3. ",
+        "Developing key features for the Bikemaps web application using Django, PostgreSQL, Leaftlet.",
+        "Planning and integrating new features and web application architecture for MobilityAccess(BikeMaps pedestrian) using React, Django & PostgreSQL.",
+      ],
+    },
+    {
+      CompanyName:
+        "Office of the Alumni Affairs, Memorial University of Newfoundland",
+      specialization: "Database Assistant",
+      start: "January, 2018",
+      end: "April, 2018",
+      tasks: [
+        "Achieved 18% increase in loading speed by upgrading and optimizing current web application to use Python 3, Django 3. ",
+        "Developing key features for the Bikemaps web application using Django, PostgreSQL, Leaftlet.",
+        "Planning and integrating new features and web application architecture for MobilityAccess(BikeMaps pedestrian) using React, Django & PostgreSQL.",
+      ],
+    },
   ],
-  "contact": [
+  portfolio: [
     {
-      "image": "images/logo.png"
-    }
-  ]
+      name: "ACTIVITY TRACKER",
+      description:
+        "A Java Desktop Application built using the Swing library as a school project.",
+      imgurl: "images/running.jpeg",
+      link: "https://github.com/Zahin430/cs2005",
+    },
+    {
+      name: "VISITNL",
+      description:
+        "A NodeJS Web Application serves as a social media platform. Built using NodeJS and MongoDB",
+      imgurl: "images/portfolio/project2.jpg",
+      link: "https://github.com/Zahin430/VISITNL",
+    },
+    {
+      name: "RecipeBook",
+      description:
+        "An Angular Web Application that keeps a track of recipes being bought and sold.",
+      imgurl: "images/angular.png",
+      link: "https://github.com/Zahin430/RecipeBook",
+    },
+    {
+      name: "Neurofog Website",
+      description:
+        "A React application built for the Neurofog Laboratory at Memorial University of Newfoundland",
+      imgurl: "images/logo.png",
+      link: "http://neurofog.ca/",
+    },
+  ],
+  contact: [
+    {
+      image: "images/logo.png",
+    },
+  ],
+};
 
-}
-
-export default resumeData
+export default resumeData;
