e6507c55b585cad6ce8c94a51e8b31355e6c6e03
Zahin430 <zhprangon@mun.ca>
2021-01-18 23:02:24 -0330

    Updates

--- precache-manifest.e81d9841e0d65b748b90dccde18c5f06.js
+++ precache-manifest.e81d9841e0d65b748b90dccde18c5f06.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "337c963ae0c723515dd80050be1efa5a",
+    "revision": "b0dc81af427ba479924b09c6e95aa870",
     "url": "/Zahin-Hasan-Prangon/index.html"
   },
   {
-    "revision": "9febad73f94086c57151",
+    "revision": "7e526a19a15b39564277",
     "url": "/Zahin-Hasan-Prangon/static/css/main.f7ec9b6e.chunk.css"
   },
   {
-    "revision": "d23a2b82f392150f2068",
-    "url": "/Zahin-Hasan-Prangon/static/js/2.0047c582.chunk.js"
+    "revision": "5fd54a86bf720fa7f9b8",
+    "url": "/Zahin-Hasan-Prangon/static/js/2.fdb245f9.chunk.js"
   },
   {
-    "revision": "9febad73f94086c57151",
-    "url": "/Zahin-Hasan-Prangon/static/js/main.6b59434c.chunk.js"
+    "revision": "7e526a19a15b39564277",
+    "url": "/Zahin-Hasan-Prangon/static/js/main.26746368.chunk.js"
   },
   {
     "revision": "d1c5aec3373208a057dc",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Zahin-Hasan-Prangon/precache-manifest.d7b4c2f1b9da1e59bab8c95b5ee03983.js"
+  "/Zahin-Hasan-Prangon/precache-manifest.e81d9841e0d65b748b90dccde18c5f06.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Zahin-Hasan-Prangon/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
